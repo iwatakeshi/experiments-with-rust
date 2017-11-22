1a43b56a4f451851bfc36b1bbff90ddca379d11c
Takeshi <iwatakeshi@gmail.com>
2017-11-22 16:14:17 -0700

    Initial Commit

---
+++ concurrency/thread_spawn/src/main.rs
@@ -0,0 +1,18 @@
+use std::thread;
+use std::time;
+
+fn main() {
+  let t = time::Duration::from_millis(5000);
+
+  thread::spawn(|| {
+    for i in 1..10 {
+      println!("hi number {} from the spawned thread!", i);
+      thread::sleep(time::Duration::from_millis(5000));
+    }
+  });
+
+  for i in 1..20 {
+    println!("hi number {} from the main thread!", i);
+    thread::sleep(t);
+  }
+}
