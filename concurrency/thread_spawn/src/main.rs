13fa167b8258c1e1ece866292a67dcf09dcd5fc8
Takeshi <iwatakeshi@gmail.com>
2017-11-23 14:18:41 -0700

    Update

--- concurrency/thread_spawn/src/main.rs
+++ concurrency/thread_spawn/src/main.rs
@@ -2,17 +2,17 @@ use std::thread;
 use std::time;
 
 fn main() {
-  let t = time::Duration::from_millis(5000);
+  //let t = time::Duration::from_millis(5000);
 
   thread::spawn(|| {
     for i in 1..10 {
       println!("hi number {} from the spawned thread!", i);
-      thread::sleep(time::Duration::from_millis(5000));
+      //thread::sleep(time::Duration::from_millis(5000));
     }
   });
 
-  for i in 1..20 {
+  for i in 1..2000 {
     println!("hi number {} from the main thread!", i);
-    thread::sleep(t);
+    //thread::sleep(t);
   }
 }
